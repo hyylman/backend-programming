51307d8948a2156d48dd9289becc2db067dae58b
Zulfikar Hilman P <52442172+hilmanpry@users.noreply.github.com>
2023-10-19 22:54:33 +0700

    Update animal.php

--- animal.php
+++ animal.php
@@ -4,17 +4,21 @@
 class Animal
 {
     # property animals
+    public $animals;
 
     # method constructor - mengisi data awal
     # parameter: data hewan (array)
     public function __construct($data)
     {
+        $this->animals = array($data);
     }
 
     # method index - menampilkan data animals
     public function index()
     {
         # gunakan foreach untuk menampilkan data animals (array)
+        foreach($this->animals as $anml){
+            echo $anml. "<br>";
     }
 
     # method store - menambahkan hewan baru
@@ -22,12 +26,14 @@ class Animal
     public function store($data)
     {
         # gunakan method array_push untuk menambahkan data baru
+        array_push($this->animals, $data);
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
     public function update($index, $data)
     {
+        $this->animals[$index] = $data;
     }
 
     # method delete - menghapus hewan
@@ -35,6 +41,7 @@ class Animal
     public function destroy($index)
     {
         # gunakan method unset atau array_splice untuk menghapus data array
+        unset($this->animals[$index]);
     }
 }
 
