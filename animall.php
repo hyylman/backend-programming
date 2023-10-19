fa50dd33d6210777ba2c48855fdba605b6b35db1
Zulfikar Hilman P <52442172+hilmanpry@users.noreply.github.com>
2023-10-19 23:02:47 +0700

    Create animall.php

---
+++ animall.php
@@ -0,0 +1,75 @@
+<?php
+
+# membuat class Animal
+class Animal
+{
+    # property animals
+    public $animals;
+
+    # method constructor - mengisi data awal
+    # parameter: data hewan (array)
+    public function __construct()
+    {
+        $this->animals = array();
+    }
+
+    # method index - menampilkan data animals
+    public function index()
+   {
+      if (!empty($this->animals)) {
+        foreach ($this->animals as $anml) {
+            echo $anml . "<br>";
+        }
+      } else {
+        echo "Tidak ada hewan.";
+      }
+   }
+
+
+    # method store - menambahkan hewan baru
+    # parameter: hewan baru
+    public function store($data)
+    {
+        # gunakan method array_push untuk menambahkan data baru
+        array_push($this->animals, $data);
+    }
+
+    # method update - mengupdate hewan
+    # parameter: index dan hewan baru
+    public function update($index, $data)
+    {
+        $this->animals[$index] = $data;
+    }
+
+    # method delete - menghapus hewan
+    # parameter: index
+    public function destroy($index)
+    {
+       if (isset($this->animals[$index])) {
+          array_splice($this->animals, $index, 1);
+       }
+    }
+
+
+# membuat object
+# kirimkan data hewan (array) ke constructor
+$animal = new Animal([]);
+
+echo "Index - Menampilkan seluruh hewan <br>";
+$animal->index();
+echo "<br>";
+
+echo "Store - Menambahkan hewan baru <br>";
+$animal->store('burung');
+$animal->index();
+echo "<br>";
+
+echo "Update - Mengupdate hewan <br>";
+$animal->update(0, 'Kucing Anggora');
+$animal->index();
+echo "<br>";
+
+echo "Destroy - Menghapus hewan <br>";
+$animal->destroy(1);
+$animal->index();
+echo "<br>";
